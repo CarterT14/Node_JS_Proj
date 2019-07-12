f4bac0c76ea4c6311259d96400dd00569e6d53ac
Carter Thornton <carterth@buffalo.edu>
2019-07-12 12:35:19 -0400

    working with .get and .post requests in my app

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json()); //adding and using middleware
+
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'}
+
+];
+
 app.get('/', (req, res) => {
-    res.send('Hello World!!!')
-;});
+    res.send('Hello World!!!');
+});
 
 app.get('/api/courses', (req,res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
+});
+
+app.post('/api/courses', (req,res) => {
+    const course = {
+        id: courses.length + 1, 
+        name: req.body.name 
+    };
+    courses.push(course);
+    res.send(course);
 });
 
 
-app.listen(3000, () => console.log('Listenning on port 3000....'));
+
+
+
+
+app.get('/api/courses/:id' , (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id)); 
+    if (!course) res.status(404).send('The couse with the given ID as not found.');
+    res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
